486e5bded4501e7f3a056902e3d44de6cc64d267
Shogo Kawahara <kawahara@bucyou.net>
2010-08-20 02:04:46 +0900

    言語設定変更時に、即時に適用するようにする

--- lib/form/MemberConfigForm/MemberConfigLanguageForm.class.php
+++ lib/form/MemberConfigForm/MemberConfigLanguageForm.class.php
@@ -18,4 +18,19 @@
 class MemberConfigLanguageForm extends MemberConfigForm
 {
   protected $category = 'language';
+
+  public function save()
+  {
+    if ($language = $this->getValue('language'))
+    {
+      sfContext::getInstance()->getUser()->setCulture($language);
+    }
+
+    if ($timezone = $this->getValue('time_zone'))
+    {
+      sfContext::getInstance()->getUser()->setTimezone($timezone);
+    }
+
+    return parent::save();
+  }
 }
